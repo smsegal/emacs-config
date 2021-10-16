05951e3fd7e1e8925cb5a8632ab73e4c64a1f309
Shane Segal <shane@smsegal.ca>
2021-10-16 00:19:55 -0400

    update to modern vertico settings

--- modules/+minibuffer.el
+++ modules/+minibuffer.el
@@ -3,32 +3,15 @@
 (require '+project)
 
 ;; Incremental Completion
-;; We're using Selectrum and the associated ecosystem. They have a bigger
+;; We're using vertico and the associated ecosystem. They have a bigger
 ;; focus on creating and extending basic APIs vs alternative like Ivy.
 
 (use-package vertico
   :hook (after-init . vertico-mode)
   :general
-  (general-imap "C-k" nil)
-  (:keymaps 'vertico-map
-   "C-j" 'vertico-next
-   "C-k" 'vertico-previous)
-  :init
-  ;; Add prompt indicator to `completing-read-multiple'.
-  (defun crm-indicator (args)
-    (cons (concat "[CRM] " (car args)) (cdr args)))
-  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
-
-  ;; Grow and shrink minibuffer
-  ;; (setq resize-mini-windows t)
-
-  ;; Do not allow the cursor in the minibuffer prompt
-  (setq minibuffer-prompt-properties
-        '(read-only t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-
-  ;; Enable recursive minibuffers
-  (setq enable-recursive-minibuffers t))
+  (:keymaps 'minibuffer-local-map
+   "C-j" 'next-line
+   "C-k" 'previous-line))
 
 ;; Persist history over Emacs restarts. Vertico sorts by history position.
 (use-package savehist
@@ -41,33 +24,18 @@
         completion-category-defaults nil
         completion-category-overrides '((file (styles . (partial-completion))))))
 
-;; Consult is to selectrum as counsel is to Ivy.
-;; Marginalia is a bit of extra eye-candy on top of Consult.
 (use-package consult
   :init
-  ;; Replace functions (consult-multi-occur is a drop-in replacement)
-  (fset 'multi-occur #'consult-multi-occur)
-  (fset 'goto-line #'consult-goto-line)
-  :config
-  ;; Configure register preview function.
-  ;; This gives a consistent display for both `consult-register' and
-  ;; the register preview when editing registers.
-  (setq register-preview-delay 0
-        register-preview-function #'consult-register-preview)
-
-  ;; Optionally tweak the register preview window.
-  ;; This adds zebra stripes, sorting and hides the mode line of the window.
-  (advice-add #'register-preview :override #'consult-register-window)
-
+  (advice-add #'completing-read-multiple :override #'consult-completing-read-multiple)
   (setq xref-show-xrefs-function #'consult-xref
-        xref-show-definitions-function #'consult-xref)
+	xref-show-definitions-function #'consult-xref)
+  :config
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any))
 
-  ;;project root
-  ;; (setq consult-project-root-function
-  ;;       (lambda () (locate-dominating-file "." ".git")))
   (setq consult-project-root-function
-        (lambda ()
-          (when-let (project (project-current))
+	(lambda ()
+	  (when-let (project (project-current))
             (car (project-roots project)))))
   :general
   (:prefix-map 'help-map
@@ -98,35 +66,79 @@
    "g" #'affe-grep
    "f" #'affe-find)
   :config
-  ;; Configure Orderless
-  (setq affe-regexp-function #'orderless-pattern-compiler
-        affe-highlight-function #'orderless-highlight-matches)
+  (defun affe-orderless-regexp-compiler (input _type)
+    (setq input (orderless-pattern-compiler input))
+    (cons input (lambda (str) (orderless--highlight input str))))
+  (setq affe-regexp-compiler #'affe-orderless-regexp-compiler)
 
   ;; Manual preview key for `affe-grep'
   (consult-customize affe-grep :preview-key (kbd "M-.")))
 
 (use-package embark
-  :config
-  (setq embark-action-indicator
-        (lambda (map _target)
-          (which-key--show-keymap "Embark" map nil nil 'no-paging)
-          #'which-key--hide-popup-ignore-command)
-        embark-become-indicator embark-action-indicator)
   :general
-  ("C-S-a" #'embark-act))
+  (:states '(normal insert)
+   :keymaps 'global
+   "C-." #'embark-act
+   "C-;" #'embark-dwim)
+  (:keymaps 'help-map
+   "B" #'embark-bindings)
+  :init
+  (setq prefix-help-command #'embark-prefix-help-command)
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+	       '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+		 (window-parameters (mode-line-format . none))))
+  (defun embark-which-key-indicator ()
+    "An embark indicator that displays keymaps using which-key.
+The which-key help message will show the type and value of the
+current target followed by an ellipsis if there are further
+targets."
+    (lambda (&optional keymap targets prefix)
+      (if (null keymape
+		(which-key--hide-popup-ignore-command)
+		(which-key--show-keymap
+		 (if (eq (caar targets) 'embark-become)
+		     "Become"
+		   (format "Act on %s '%s'%s"
+			   (plist-get (car targets) :type)
+			   (embark--truncate-target (plist-get (car targets) :target))
+			   (if (cdr targets) "…" "")))
+		 (if prefix
+		     (pcase (lookup-key keymap prefix 'accept-default)
+		       ((and (pred keymapp) km) km)
+		       (_ (key-binding prefix 'accept-default)))
+		   keymap)
+		 nil nil t)))))
+
+  (setq embark-indicators
+	'(embark-which-key-indicator
+	  embark-highlight-indicator
+	  embark-isearch-highlight-indicator)))
 
 (use-package embark-consult
   :after (embark consult)
   :demand t
-  :hook
-  (embark-collect-mode . embark-consult-preview-minor-mode))
+  :hook (embark-collect-mode . consult-preview-at-point-mode))
+
+(use-package emacs
+  :straight nil
+  :init
+  (defun crm-indicator (args)
+    (cons (concat "[CRM] " (car args)) (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  (setq minibuffer-prompt-properties
+	'(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  (when (>= emacs-major-version 28)
+    (setq read-extended-command-predicate #'command-completion-default-include-p))
+
+  (setq enable-recursive-minibuffers t))
 
 (use-package marginalia
-  :commands (marginalia-mode)
-  :init
-  (marginalia-mode)
-
-  ;; use heavy annotators (keybindings, descriptions etc.)
-  (setq marginalia-annotators '(marginalia-annotators-heavy)))
+  :init (marginalia-mode))
 
 (provide '+minibuffer)
